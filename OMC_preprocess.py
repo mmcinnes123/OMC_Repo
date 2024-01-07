c32db5422f02b078df14eb5feaf2d0e3891abf56
mmcinnes123 <118896958+mmcinnes123@users.noreply.github.com>
2024-01-07 12:04:14 +0000

    Small file variable name changes.

--- OMC_preprocess.py
+++ OMC_preprocess.py
@@ -5,12 +5,12 @@
 from OMC_IK_functions import *
 
 """ SETTINGS """
-folder_name = r'C:\Users\r03mm22\Documents\Protocol_Testing\Tests\23_12_20\RawData'   # Path to the raw data files
-input_file_name = folder_name + r'\20thDec_Movements - Report1 - Marker_Traj.txt'
+raw_data_dir = r'C:\Users\r03mm22\Documents\Protocol_Testing\Tests\23_12_20\RawData'   # Path to the raw data files
+input_file_name = raw_data_dir + r'\20thDec_Movements - Report1 - Marker_Traj.txt'
 sample_rate = 100
 
 # Convert MM .txt file to .trc and save .trc file
-MM_2_trc(input_file_name, sample_rate, output_file_name=folder_name.replace(r'\RawData', '') + r'\BL_marker_pos.trc')
+MM_2_trc(input_file_name, sample_rate, output_file_name=raw_data_dir.replace(r'\RawData', '') + r'\BL_marker_pos.trc')
 
 # Preview marker data and check time for static pose
 print("\nPreview data .trc file. \n Set static pose time in OMC_IK script.")
