e5c32c8877e1a1de2976bc791dc6424a2430787f
mmcinnes123 <118896958+mmcinnes123@users.noreply.github.com>
2024-05-21 08:32:43 +0100

    Tidied OMC_preprocess.py.

--- OMC_preprocess.py
+++ OMC_preprocess.py
@@ -2,39 +2,38 @@
 # Input is a MM report .txt file
 # Output is a .trc file
 
-from OMC_IK_functions import *
+from OMC_helpers import MM_2_trc
+from constants import sample_rate
+
 import os
 
 """ SETTINGS """
 
 # Quick Settings
-subject_code = 'P4'
+subject_code_list = ['P4']
 trial_name_list = ['CP', 'JA_Slow', 'JA_Fast', 'ROM', 'ADL']
-sample_rate = 100
 
 
 """ MAIN """
 
-# Create/specify some file directories
-parent_dir = r'C:\Users\r03mm22\Documents\Protocol_Testing\2024 Data Collection' + '\\' + subject_code
-OMC_dir = parent_dir + r'\OMC'
-if os.path.exists(OMC_dir) == False:
-    os.mkdir(OMC_dir)
-output_dir = OMC_dir + r'\OMC_trcs'
-if os.path.exists(output_dir) == False:
-    os.mkdir(output_dir)
 
 # Iterate through the collection of movement types
-for trial_name in trial_name_list:
+for subject_code in subject_code_list:
+    for trial_name in trial_name_list:
 
-    print(f'\nWriting {trial_name}_marker_pos.trc...')
+        # Define some file directories
+        parent_dir = os.path.join(r'C:\Users\r03mm22\Documents\Protocol_Testing\2024 Data Collection', subject_code)
+        OMC_dir = os.path.join(parent_dir, 'OMC')
+        output_dir = OMC_dir + r'\OMC_trcs'
+        input_file_name = parent_dir + r'\RawData' + r'\\' + subject_code + '_' + trial_name + r' - Report1 - Marker_Traj.txt'
+        output_file_name = output_dir + r'\\' + trial_name + '_marker_pos.trc'
+        # Create folders if they don't already exist
+        os.makedirs(OMC_dir, exist_ok=True)
+        os.makedirs(output_dir, exist_ok=True)
 
-    input_file_name = parent_dir + r'\RawData' + r'\\' + subject_code + '_' + trial_name + r' - Report1 - Marker_Traj.txt'
-    output_file_name = output_dir + r'\\' + trial_name + '_marker_pos.trc'
-
-    # Convert MM .txt file to .trc and save .trc file
-    MM_2_trc(input_file_name, sample_rate, output_file_name)
-
-    print(f'\nWritten {trial_name}_marker_pos.trc')
+        # Convert MM .txt file to .trc file
+        print(f'\nWriting {trial_name}_marker_pos.trc...')
+        MM_2_trc(input_file_name, sample_rate, output_file_name)
+        print(f'\nWritten {trial_name}_marker_pos.trc')
 
 
---
+++ constants.py
@@ -0,0 +1,3 @@
+# This script contains a list of constant settings used throughout the repo
+
+sample_rate = 100
